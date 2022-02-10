aa114fe660e24b7a2769a4a62119e6dbdb307904
takahara tomoaki <jihyun8286@gmail.com>
2022-02-10 12:11:13 +0900

    画像を各人のものにし、サイズを統一した

--- 1stprize.js
+++ 1stprize.js
@@ -10,12 +10,12 @@
 
 // C's zone
 const img = [
-    "./img_C/tuyoshi.png",
-    "./img_C/tuyoshi.png",
-    "./img_C/tuyoshi.png",
-    "./img_C/tuyoshi.png",
-    "./img_C/tuyoshi.png",
-    "./img_C/tuyoshi.png",
+    "./img_C/kento.png",
+    "./img_C/ponta.png",
+    "./img_C/kazunori.png",
+    "./img_C/keisiro.png",
+    "./img_C/kotoki.png",
+    "./img_C/momo.png",
     "./img_C/tuyoshi.png",
 ];
 const memberName = [
@@ -29,26 +29,26 @@ const memberName = [
 ]
 const price = [
     "１億",
-    "１億",
-    "１億",
-    "１億",
-    "１億",
-    "１億",
-    "１億",
+    "２億",
+    "３億",
+    "４億",
+    "５億",
+    "６億",
+    "７億",
 ];
-const a = {
-    
-}
+
 
 let contents = ""
-console.log(img.length)
+
 for (let i=img.length-1; i>=0; i--) {
-    contents += `<li class="member_C">`
-            +`    <a href="./">`
-            +`        <img class="member_img_C" src="${img[i]}" alt="はにゃ">`
+    contents += `<li id="${i+1}" class="member_C">`
+            +`    <a href="./html/1_C.html?id=${i+1}">`
+            +`        <div class="member_img_wrap_C">`
+            +`          <img class="member_img_C" src="${img[i]}" alt="はにゃ">`
+            +`        </div>`
             +`        <p class="member_name_C">${memberName[i]}</p>`
-            +`        <p class="member_price_C">${price[i]} + tax</p>`
-            +`    </a>`
+            +`        <p class="member_price_C">¥${price[i]} +tax</p>`
+            +`    </div>`
             +`</li>`
 };
 
